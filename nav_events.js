493293eee50b3fa94a034e527d1312dcbdc6008c
Miles Wirht <regime_skylark.0y@icloud.com>
2024-12-06 20:29:15 -0500

    Mended the failure to revert to state after opening nav

--- nav_events.js
+++ nav_events.js
@@ -22,6 +22,7 @@ function display_menu() {
 		for (var i = 0; i < bodyChildren.length; i++) {
 			if (bodyChildren[i].tagName.toLowerCase() !== "nav") {
 				bodyChildren[i].style.opacity = "1";
+				bodyChildren[i].style.display = "inherit";
 			}
 		}
 		document.querySelector("nav > ul").style.display = "";
